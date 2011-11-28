10686ff1783a73bf8c1c858a1a5239f4967d20b0
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2011-11-28 20:41:46 +0100

    threw out all the nonsense

--- src/bs/implementations/Simulation.java
+++ src/bs/implementations/Simulation.java
@@ -2,35 +2,22 @@ package bs.implementations;
 
 public class Simulation {
 	
-	public static Simulation create() {
-		return new Simulation();
-	}
-	private Table table;
-	private static Smoker smoker1;
-	private Smoker smoker2;
-	private Smoker smoker3;
-	private static Agent agent;
-	
-	private Simulation() {
-		this.table = Table.create(0, 0, 0);
-		this.smoker1 = Smoker.create(table, 1, 0, 0);
-		this.smoker2 = Smoker.create(table, 0, 1, 0);
-		this.smoker3 = Smoker.create(table, 0, 0, 1);
-		this.agent = Agent.create(table, 40, 40, 40);
-	}
-	
 	public static void main(String[] args) {
-		Simulation simulation = Simulation.create();
-		long runtime = System.currentTimeMillis() + 10000;
 		try {
+			System.out.println("Entering simulation");
+			Table table = Table.create(0, 0, 0);
+			Smoker smoker1 = Smoker.create(table, 1, 0, 0);
+			Smoker smoker2 = Smoker.create(table, 0, 1, 0);
+			Smoker smoker3 = Smoker.create(table, 0, 0, 1);
+			Agent agent = Agent.create(table, 40, 40, 40);
 			
-			smoker1.start();
 			agent.start();
-			while (System.currentTimeMillis() > runtime) {
-				System.exit(0);
-			}
+			smoker1.start();
+			smoker2.start();
+			smoker3.start();
 		} catch (Exception e) {
 			System.out.println(e);
+			System.exit(0);
 		}
 	}
 }
